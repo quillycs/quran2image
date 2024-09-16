d20eff241fe1af4bca7895330264adeea4556b77
LuayB <luaboett@gmail.com>
2024-09-16 18:08:08 +0100

    Fetch Surah name and display it alongside the verse key under the translation

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function generateVerse() {
     const url = 'https://api.quran.com/api/v4/verses/random';
+    let verse_key;
 
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        const verse_key = data.verse.verse_key;
+        verse_key = data.verse.verse_key;
         const url2 = 'https://api.quran.com/api/v4/quran/translations/20?verse_key=' + verse_key;
 
         return fetch(url2);
@@ -14,8 +15,15 @@ function generateVerse() {
     .then(data => {
         const verse_translation = data.translations[0].text;
         document.getElementById('verse-paragraph').innerHTML = verse_translation;
+        
+        const url3 = 'https://api.quran.com/api/v4/chapters/' + verse_key;
+
+        return fetch(url3);
     })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+
+    .then(response => response.json())
+    .then(data => {
+        const chapter_name = data.chapter.name_simple;
+        document.getElementById('verse-paragraph').innerHTML += '<br>- Surah ' + chapter_name + ' (' + verse_key + ')';
+    })
 }
