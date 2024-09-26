34b143dc2af4d350576673528cdf38b030d7b295
LuayB <luaboett@gmail.com>
2024-09-26 22:38:49 +0100

    Display the ayah in Arabic using glyphs

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 function generateVerse() {
-    const url = 'https://api.quran.com/api/v4/verses/random';
+    let glyph_text;
+    let verse_page_number;
+    let verse_text;
     let verse_key;
-    let verse_translation;
     let chapter_name;
+
+    const url = 'https://api.quran.com/api/v4/verses/random';
 
     fetch(url)
     .then(response => response.json())
     .then(data => {
         verse_key = data.verse.verse_key;
+        verse_page_number = data.verse.page_number.toString();
         const url2 = 'https://api.quran.com/api/v4/quran/translations/20?verse_key=' + verse_key;
         return fetch(url2);
     })
     
     .then(response => response.json())
     .then(data => {
-        verse_translation = data.translations[0].text;        
+        verse_text = data.translations[0].text;        
         const url3 = 'https://api.quran.com/api/v4/chapters/' + verse_key;
         return fetch(url3);
     })
@@ -22,6 +26,24 @@ function generateVerse() {
     .then(response => response.json())
     .then(data => {
         chapter_name = data.chapter.name_simple;
-        document.getElementById('verse-paragraph').innerHTML = verse_translation + '<br>- Surah ' + chapter_name + ' (' + verse_key + ')';
+        const url4 = 'https://api.quran.com/api/v4/quran/verses/code_v1?verse_key=' + verse_key;
+        return fetch(url4);
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        glyph_text = data.verses[0].code_v1; 
+
+        console.log("Name: [" + verse_page_number.toString() + "]");
+
+        const fontFace = new FontFace("custom_font", `url(fonts/QCF_P${verse_page_number.toString().padStart(3, '0')}.TTF)`);
+        document.fonts.add(fontFace);
+        
+        fontFace.load().then((loadedFace) => {
+            document.getElementById('glyph_text').style.fontFamily = loadedFace.family;
+        })
+        
+        document.getElementById('glyph_text').innerHTML = glyph_text;  
+        document.getElementById('verse_text').innerHTML = verse_text + '<br>- Surah ' + chapter_name + ' (' + verse_key + ')';
     })
 }
